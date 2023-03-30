c32d924fc2bab27c72be7ec34d12ef60283143f5
andres quintero <bodysystemsforever@gmail.com>
2023-03-30 00:14:02 -0500

    entregando will

--- 01.js
+++ 01.js
@@ -11,7 +11,14 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
+  var arraResultante =[];
 
+  for(i=0; i< array.length; i++){
+    if(typeof array[i]==="number"){
+      arraResultante.push(array[i])
+    }
+  }
+  return arraResultante;
 }
 
 // No modifiques nada debajo de esta linea //
--- 03.js
+++ 03.js
@@ -14,7 +14,13 @@ function buscarAmigo(amigos, nombre) {
   //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
 
   // Tu código aca:
-  
+  var amigoEncontrado={};
+  for(var i =0; i < amigos.length; i++){
+    if(amigos[i].nombre === nombre){
+      amigoEncontrado = amigos[i];
+    }
+  }
+  return amigoEncontrado;
 }
 
 // No modifiques nada debajo de esta linea //
